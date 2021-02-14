7e71d3f687ed885888025fbf62cc01a80bcf9325
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-14 02:30:32 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,18 +14,7 @@ def link
 end
 
 def all_phrases
-  def mario
-    phrase = "It's-a me, Mario!"
-    puts phrase
-  end
-
-  def toadstool
-    status = "Thank You Mario! But Our Princess Is In Another Castle!"
-    puts status
-  end
-
-  def link
-    phrase = "It's Dangerous To Go Alone! Take This."
-    puts phrase
-  end
+ puts .mario
+ puts .toadstool
+ puts .link
 end
