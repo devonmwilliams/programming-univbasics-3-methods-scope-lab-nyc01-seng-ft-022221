a8d8768bc146e651f9f88b1ab5b86b3518ed9cdc
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-14 02:22:41 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -12,3 +12,20 @@ def link
   phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
 end
+
+def all_phrases
+  def mario
+    phrase = "It's-a me, Mario!"
+    puts phrase
+  end
+
+  def toadstool
+    status = "Thank You Mario! But Our Princess Is In Another Castle!"
+    puts status
+  end
+
+  def link
+    phrase = "It's Dangerous To Go Alone! Take This."
+    puts phrase
+  end
+end
